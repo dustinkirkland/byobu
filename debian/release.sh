5e6972ebbfb5f9cb680ea799b2a3c14835423392
Dustin Kirkland <dustin.kirkland@gmail.com>
2009-04-08 11:46:37 -0700

    use bzr bd to build, rather than debuild

--- debian/release.sh
+++ debian/release.sh
@@ -8,18 +8,17 @@ error() {
 head -n1 debian/changelog | grep "unreleased" || error "This version must be 'unreleased'"
 
 i=1
-./debian/rules get-orig-source
-debuild
+bzr bd
 sed -i "s/) unreleased;/-0ubuntu1~ppa1) hardy;/" debian/changelog
-debuild -S
+bzr bd -S
 sed -i "s/ppa1) hardy;/ppa2) intrepid;/" debian/changelog
-debuild -S
+bzr bd -S
 sed -i "s/ppa2) intrepid;/ppa3) jaunty;/" debian/changelog
-debuild -S
+bzr bd -S
 sed -i "s/~ppa3) jaunty;/) jaunty;/" debian/changelog
-debuild -S
-ver=`head -n1 debian/changelog | sed "s/^.*(1.//" | sed "s/-.*$//"`
-ver=`expr $ver + 1`
+bzr bd -S
+curver=`head -n1 debian/changelog | sed "s/^.*(1.//" | sed "s/-.*$//"`
+ver=`expr $curver + 1`
 dch -v "1.$ver" "UNRELEASED"
 sed -i "s/1.$ver) jaunty;/1.$ver) unreleased;/" debian/changelog
 
@@ -33,6 +32,7 @@ echo "  dput screen-profiles-ppa ../*ppa*changes"
 echo
 echo "To commit and push:"
 echo "  bzr diff"
+echo "  bzr tag 1.$curver"
 echo "  bzr commit && bzr push"
 echo
 echo
