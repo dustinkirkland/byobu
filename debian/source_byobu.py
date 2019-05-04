67a2bed59799af17f4c62a831b6af1ac40a68832
Paride Legovini <paride.legovini@canonical.com>
2019-05-04 16:57:13 +0200

    Do not include the user's .screenrc in the apport generated crash report
    Fixes LP: #1827202 (CVE-2019-7306).

--- debian/source_byobu.py
+++ debian/source_byobu.py
@@ -10,4 +10,3 @@ from os import path
 def add_info(report):
     attach_related_packages(report, ['byobu*', 'screen*'])
     report['Binaries'] = command_output(['sh', '-c', 'ls -alF /usr/bin/screen* /usr/bin/byobu*'])
-    attach_file_if_exists(report, path.expanduser('~/.screenrc'), 'ScreenRC')
