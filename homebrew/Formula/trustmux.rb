47b6ca78b1797a7a73dbd1fde2ea343c7c7c2f2a
Dustin Kirkland <dustin.kirkland@gmail.com>
2026-05-28 04:18:28 -0500

    homebrew: update trustmux formula to 7.0 PyPI release

--- homebrew/Formula/trustmux.rb
+++ homebrew/Formula/trustmux.rb
@@ -4,17 +4,12 @@ class Trustmux < Formula
   desc "Monitor and interact with tmux/Byobu sessions from your phone"
   homepage "https://trustmux.app"
 
-  # Stable: GitHub archive of the feature/byobu-mobile branch HEAD.
-  # TODO: replace with PyPI sdist URL once published to PyPI:
-  #   url "https://files.pythonhosted.org/packages/.../trustmux-7.0.0.tar.gz"
-  #   sha256 "42e9963f071df472230d41a14523b5838432947639becdbf62fdb5a03362292f"
-  # (also remove the `cd "mobile"` wrapper in the install block below)
-  url "https://github.com/dustinkirkland/byobu/archive/cddc428a27446d4e6d4320e96928f9b4309aeb61.tar.gz"
-  sha256 "2d7597a62702b5daea80b38d71b5a056333172fc6ea2f1f89fe5a2fc91515fdf"
-  version "7.0.0"
+  url "https://files.pythonhosted.org/packages/fd/d7/2950498369e93cbd301fe8d528efe3a5c248d263164c1ac69fc9d950a11e/trustmux-7.0.tar.gz"
+  sha256 "61bb6de895226595d9a99936f41965b3421e55b826474f572b5aefabbab969e9"
+  version "7.0"
   license "GPL-3.0-or-later"
 
-  head "https://github.com/dustinkirkland/byobu.git", branch: "feature/byobu-mobile"
+  head "https://github.com/dustinkirkland/byobu.git", branch: "master"
 
   depends_on "python@3.12"
   depends_on "tmux"
@@ -25,12 +20,7 @@ class Trustmux < Formula
   end
 
   def install
-    # Package root is in mobile/ subdirectory of the byobu repo.
-    # Once the formula is updated to reference a PyPI sdist, remove this cd block
-    # (PyPI sdists extract with pyproject.toml at the top level).
-    cd "mobile" do
-      virtualenv_install_with_resources
-    end
+    virtualenv_install_with_resources
   end
 
   test do
